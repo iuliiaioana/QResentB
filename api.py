20fc6da07f992906cd5f71be09aa4bab8d70b9d4
laviniamnedelea <lavinia.mndl@gmail.com>
2021-11-03 12:42:12 +0200

    Added statistics comp.

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_cors import CORS, cross_origin
@@ -26,8 +28,50 @@ class Home(Resource):
     def get(self):
         return "Hello", 200
 
+
+stats_data = {} # Prezentele unei activitati, generate de qr, cu data fisei de prezenta ca si cheie. Ca valori vom avea
+# activitatea ca si cheie si ca valoare toate prezentele.
+
+class Stats(Resource):
+    def generate_statistics(self, prezenta_activitate_id):
+        act_stat = defaultdict(list, {k: 0 for k in ('inceput', 'aleator', 'final')})
+        prezenta_act = PrezentaActivitate.query.get(prezenta_activitate_id)
+        act = Activitati.query.get(prezenta_act.id_activitate)
+
+        start_treshold = (int(act.interval[3:5])- int(act.interval[0:2]))*6 # Primele minute care sunt csd "inceputul activitatii"
+        end_treshold = (int(act.interval[3:5]) - int(act.interval[0:2]))*54 # Ultimele minute care sunt csd "sfarsitul activitatii"
+        minut_generare =  (int(prezenta_act.ora_generare[0:2])-int(act.interval[0:2]))*60 + int(prezenta_act.ora_generare[3:5])
+
+        id_act = act.id
+        if id_act not in stats_data.keys():
+            stats_data[id_act] = {}
+
+        if minut_generare<=start_treshold:
+            no = db.session.query(user_prezenta).filter(user_prezenta.c.prezenta_id==prezenta_activitate_id).count()
+            if str(prezenta_act.data) not in stats_data[id_act].keys():
+                stats_data[id_act][str(prezenta_act.data)] = act_stat
+            stats_data[id_act][str(prezenta_act.data)]['inceput'] = no
+
+        elif minut_generare>=end_treshold:
+            no = db.session.query(user_prezenta).filter(user_prezenta.c.prezenta_id==prezenta_activitate_id).count()
+            if str(prezenta_act.data) not in stats_data[id_act].keys():
+                stats_data[id_act][str(prezenta_act.data)] = act_stat
+            stats_data[id_act][str(prezenta_act.data)]['final'] = no
+
+        else:
+            no = db.session.query(user_prezenta).filter(user_prezenta.c.prezenta_id==prezenta_activitate_id).count()
+            if str(prezenta_act.data) not in stats_data[id_act].keys():
+                stats_data[id_act][str(prezenta_act.data)] = act_stat
+            stats_data[id_act][str(prezenta_act.data)]['aleator'] = no
+
+    def get(self):
+        return "Statistici"
+
+
 api.add_resource(Home, '/home')
 api.add_resource(Login, '/login')
+api.add_resource(Stats, '/stats')
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+import re
+
+from sqlalchemy.orm import validates
+
 from api import db
 
 user_prezenta = db.Table('User_Prezenta',
@@ -25,11 +29,17 @@ class User(db.Model):
         self.grupa = grupa
         self.prezente = prezente
 
+    @validates('email')
+    def validare_email(self, key, address):
+        if '@' not in address:
+            raise ValueError("Failed email validation.")
+        return address
+
 class PrezentaActivitate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ora_generare = db.Column(db.DateTime, nullable=False)
+    ora_generare = db.Column(db.String(100), nullable=False)
     id_activitate = db.Column(db.Integer, db.ForeignKey('activitati.id'), nullable=False)
-    data = db.Column(db.DateTime, nullable=False)
+    data = db.Column(db.String(100), nullable=False)
 
     def __init__(self, ora_generare, data):
         self.ora_generare = ora_generare
@@ -47,6 +57,13 @@ class Activitati(db.Model):
         self.interval = interval
         self.zi = zi
         self.grupa = grupa
+
+    @validates('interval')
+    def validare_interval(self, key, interval):
+        pattern = re.compile("^([0[0-9]|1[0-9]|2[0-3]):([0[0-9]|1[0-9]|2[0-3])$")
+        if not pattern.match(interval):
+            raise ValueError("Failed interval validation.")
+        return interval
 
 class Materie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
